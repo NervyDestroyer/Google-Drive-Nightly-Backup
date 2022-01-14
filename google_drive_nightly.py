b8f94ae0fd21dd1e02b31cf84b2d86617fa16c61
NervyDestroyer <nervydestroyer554@gmail.com>
2022-01-14 13:45:45 -0500

    Added sleeping until Gdrive directory is mounted

--- google_drive_nightly.py
+++ google_drive_nightly.py
@@ -1,15 +1,16 @@
 # Designed for use with Windows TaskScheduler or equivalent
 #
-# Will start Google Drive Desktop and copy files in ToSync to the specified Google Drive location
-# to start the upload process
+# Will wait for G:/ drive to be mounted by the Google Drive Desktop application that is started
+# via TaskScheduler or equivalent before starting.
 #
 # Will move files that have had their sync started to a datestamped SyncStarted_<DATETIME> folder
 #
 # Extra sync folders can be specified via extra_sync_folders.csv. These are lightweight folders
-# meant to be sync'd every night (such as a LiveSplit splits folder for example). These are
-# designed to be overwritten with any new data every night. The format of the csv is:
-#   FolderToSync,OutputLocation
-#   ex: path/to/Splits,G:/Splits
+# meant to be sync'd every night (such as a LiveSplit splits folder for example) IF ONE OF THE FILES
+# CHANGED. These are designed to have multple days worth of backups (up to the max num backups).
+# If the max num days worth of backups is met, the oldest backup is deleted. The format of the csv is:
+#   FolderToSync,OutputLocation,MaxBackups
+#   ex: path/to/Splits,G:/Splits,10
 #
 # NOTE: File upload not guaranteed to be complete upon program exit, may take multiple nightly
 # runs before the files are fully uploaded
@@ -19,6 +20,9 @@ import os
 import sys
 from datetime import datetime
 import traceback
+import filecmp
+import shutil
+import time
 
 EXTRA_SYNC_CONFIG = "./extra_sync_folders.csv"
 
@@ -33,7 +37,6 @@ def log_err(msg, errlog):
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-gdrive_exec", type=str, default=None, help="Google drive executable to run")
     parser.add_argument("-copy_location", type=str, default="G:/My Drive/", help="Location in Google Drive to copy files to start syncing")
 
     return parser.parse_args()
@@ -47,11 +50,13 @@ def main():
     try:
         args = parse_args()
 
-        # Make sure we know where gdrive desktop is
-        if(args.gdrive_exec is None):
-            raise ValueError( \
-                "Must specify Google Drive Desktop executable with -gdrive_exec argument")
+        # Wait for gdrive directory to exist (wait an additional 5 seconds afterwards just in case)
+        print("Sleeping until Gdrive directory exists...")
+        while(not os.path.exists(args.copy_location)):
+            time.sleep(1)
 
+        print("Gdrive directory found, sleeping an additional 10 seconds (for sanity)")
+        time.sleep(10)
 
     except Exception:
         formatted_exc = traceback.format_exc()
